c328f2553b85bc271365342c259e035f77249a18
Todd <37270120+HT-MA@users.noreply.github.com>
2024-06-17 16:23:14 +0800

    revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,13 @@ export default defineConfig({
 			},
 			sidebar: [
 				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
 					label: 'Kubernetes',
 					autogenerate: { directory: 'Kubernetes' },
 				},
